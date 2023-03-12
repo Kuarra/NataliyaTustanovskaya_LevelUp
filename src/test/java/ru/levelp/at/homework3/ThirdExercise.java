38c488f61b3b90e84ff5de0cd9c74b09a12ab910
Nataliya Tustanovskaya <ksaarrj@gmail.com>
2023-03-13 00:58:10 +0300

    homework_3

---
+++ src/test/java/ru/levelp/at/homework3/ThirdExercise.java
@@ -0,0 +1,98 @@
+package ru.levelp.at.homework3;
+
+import java.time.Duration;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ThirdExercise {
+
+    private static final String URL = "https://mail.ru/";
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        driver.navigate().to(URL);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    @Tag(Tags.Suites)
+    void openMailHomePage() {
+        driver.navigate().to(URL); //открыть домашнюю страницу
+        driver.findElement(By.className("resplash-btn")).click(); // кнопка Войти
+        WebElement frameElement = driver.findElement(By.className("ag-popup__frame__layout__iframe"));
+        driver.switchTo().frame(frameElement);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")))
+            .sendKeys("mailbox_testovich"); //ввод логина
+        driver.findElement(By.className("base-0-2-79")).click(); //кнопка "Ввести пароль"
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")))
+            .sendKeys("kuarra_555"); //ввод пароля
+
+        driver.findElement(By.xpath("//button[@data-test-id= 'submit-button']")).click(); //кнопка "Войти"
+        WebElement mailName = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text() = 'mailbox_testovich@mail.ru']")));
+        Assertions.assertThat(mailName.getText()).isEqualTo("mailbox_testovich@mail.ru");
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href = '/compose/']"))).click(); //написать письмо
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class = 'container--H9L5q size_s--3_M-_']")))
+            .click(); // поле "Кому"
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class = 'container--H9L5q size_s--3_M-_']")))
+            .sendKeys("mailbox_testovich@mail.ru"); // заполнение поля "Кому"
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name = 'Subject']")))
+            .sendKeys("Test"); //заполнение темы письма
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By
+                .xpath("//div[contains(@class, 'compose-app_fix')]//div[contains(@class, 'editable')]/div/div[2]")))
+            .sendKeys( "test body"); //заполнение тела письма
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@data-test-id = 'send']")))
+            .click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class= 'ico ico_16-close ico_size_s']"))).click();
+
+        //проверить что письмо в входящие
+        WebElement incoming = driver.findElement(By.xpath("//*[@href = '/tomyself/?']"));
+        incoming.click(); // открыть папку входящие
+        WebElement correspondentInc = driver.findElement(By.xpath("//*[@class = 'llct__correspondent llct__correspondent_new']"));
+        WebElement subjectInc = driver.findElement(By.xpath("//*[@class = 'll-sj__normal']"));
+        WebElement bodyInc = driver.findElement(By.xpath("//*[@class = 'llct__snippet']"));
+
+        // Assertions.assertThat(incoming.getText()).isEqualTo("Письма себе"); ?
+        //Assertions.assertThat(correspondentInc.getText()).isEqualTo("Test Testovich");??
+        Assertions.assertThat(subjectInc.getText()).isEqualTo("Test");
+        Assertions.assertThat(bodyInc.getText()).isEqualTo("test body -- Test Testovich Отправлено из Почты Mail.ru");
+
+        subjectInc.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@data-title-shortcut= 'Del']"))).click();
+
+        WebElement trash = driver.findElement(By.xpath("//*[@href = '/trash/?']"));
+        trash.click(); // открыть папку отправленные
+        WebElement correspondent1 = driver.findElement(By.xpath("//*[@class = 'llct__correspondent llct__correspondent_new']"));
+        WebElement subject1 = driver.findElement(By.xpath("//*[@class = 'll-sj__normal']"));
+        WebElement body1 = driver.findElement(By.xpath("//*[@class = 'llct__snippet']"));
+
+        //Assertions.assertThat(trash.getText()).isEqualTo("Корзина Очистить");?
+        Assertions.assertThat(correspondent1.getText()).isEqualTo("Test Testovich");
+        Assertions.assertThat(subject1.getText()).isEqualTo("Test");
+        Assertions.assertThat(body1.getText()).isEqualTo("test body -- Test Testovich Отправлено из Почты Mail.ru");
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@data-testid = 'whiteline-account']"))).click(); //нажать на почту
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@data-testid = 'whiteline-account-exit']"))).click(); //Выход
+
+
+    }
+
+
+}
+
+
+
+
